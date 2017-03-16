6fa12f1c27065c62b2afc7601d8d9f2739e8366f
Karthik Kompella <kompella.surya99@gmail.com>
2017-03-16 04:51:00 -0500

    Decision Tree
    Predictions output to a file iris.docx

---
+++ dt_author_id.py
@@ -0,0 +1,36 @@
+
+
+""" 
+    This is the code to accompany the Lesson 3 (decision tree) mini-project.
+
+    Use a Decision Tree to identify emails from the Enron corpus by author:    
+    Sara has label 0
+    Chris has label 1
+"""
+    
+import sys
+from time import time
+sys.path.append("../tools/")
+from email_preprocess import preprocess
+from sklearn import tree
+
+
+### features_train and features_test are the features for the training
+### and testing datasets, respectively
+### labels_train and labels_test are the corresponding item labels
+features_train, features_test, labels_train, labels_test = preprocess()
+
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features_train,labels_train)
+pred = clf.predict(features_test)
+
+print pred
+print clf.score(features_test, labels_test)
+
+#########################################################
+### your code goes here ###
+
+
+#########################################################
+
+
